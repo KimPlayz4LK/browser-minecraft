574a6e4977a8f757df517a46a48e8477ac4167df
KimPlayz4LK <leokim060109@gmail.com>
2022-08-30 15:38:29 -0300

    Added bot controls

--- playScript.js
+++ playScript.js
@@ -1,6 +1,7 @@
-const socket=io(`https://browser-minecraft.kimplayz4lk.repl.co/`,{transports:['websocket'],allowUpgrades:false});
+const socket=io(`http://browser-minecraft.herokuapp.com`,{transports:['websocket'],allowUpgrades:false});
 var isConnected=false;
 var html={};
+var controlsDisabled=true;
 html.username=document.querySelector("#username");
 html.serverip=document.querySelector("#serverip");
 html.serverport=document.querySelector("#serverport");
@@ -11,48 +12,53 @@ html.botUsername=document.querySelector("#botUsername");
 html.connectbutton.addEventListener("click",connectMinecraft);
 html.gameChat=document.querySelector(`#gameChat ul`);
 html.gameChatInput=document.querySelector(`#gameChatInput`);
-
+html.botStats=document.querySelector("#botStats");
+html.firstPerson=document.querySelector("#firstPersonCheck");
 function connectMinecraft(){
 if(socket.connected){
 html.errortext.innerText="Sent a Minecraft server connection request, waiting for response...";
 var username=html.username.value.replace(/[^0-9a-zA-Z_]/g,"");
 var serverip=html.serverip.value.replace(/[^0-9a-zA-Z_\-.]/g,"");
 var serverport=html.serverport.value;
-socket.emit("connectMinecraft",{username:username,serverip:serverip,serverport:serverport});
+var firstPerson=html.firstPerson.checked;
+socket.emit("connectMinecraft",{username:username,serverip:serverip,serverport:serverport,firstPerson:firstPerson});
 }else{
 html.errortext.innerText="Not connected to the server.";
 }}
-
 socket.on(`connect`,()=>{html.errortext.innerText="Connected to the server.";updateScreen();});
 socket.on(`disconnect`,()=>{isConnected=false;html.errortext.innerText="Disconnected from the server.";updateScreen();});
-
 socket.on(`minecraftConnection`,(data)=>{
 isConnected=true;
 document.title=`${data.username}@${data.server} - BrowserMinecraft`;
 html.screen.src=data.url;
-html.botUsername=data.username;
+html.botUsername.innerText=data.username;
+html.botStats.innerHTML=`${data.health.toFixed(2)} HP<br>X: ${data.xPos.toFixed(2)} Y: ${data.yPos.toFixed(2)} Z: ${data.zPos.toFixed(2)}`;
 html.errortext.innerHTML=`Connected to the Minecraft server.`;
 updateScreen();
 });
-
 socket.on(`minecraftConnectionError`,(data)=>{
 isConnected=false;
 html.errortext.innerHTML=`An error occured while connecting to the Minecraft server.<br>Reason: ${data}`;
 updateScreen();
 });
-
 socket.on(`minecraftConnectionKicked`,(data)=>{
 isConnected=false;
 html.errortext.innerHTML=`Kicked from the Minecraft server.<br>Reason: ${data}`;
 updateScreen();
 });
-
 function updateScreen(){
 if(isConnected){
 document.querySelector("#connectionMenu").style.display="none";
 document.querySelector("#game").style.display="block";
+controlsDisabled=false;
 }else{
+controlsDisabled=true;
 document.title=`Connect - BrowserMinecraft`;
+html.gameChat.innerHTML="";
+html.screen.src="";
+html.botUsername.innerText="-";
+html.botStats.innerText="-";
+html.gameChatInput.value="";
 document.querySelector("#connectionMenu").style.display="block";
 document.querySelector("#game").style.display="none";
 }}
@@ -76,5 +82,49 @@ if(isConnected){
 var message=html.gameChatInput.value;
 socket.emit("sendChatMessage",message);
 html.gameChatInput.value="";
+html.gameChatInput.blur();
 }
 }
+socket.on(`minecraftChatMessage`,(data)=>{addChatMessage("chat",data.username,data.message);});
+socket.on(`minecraftWhisperMessage`,(data)=>{addChatMessage("whisper",data.username,data.message);});
+socket.on(`minecraftPlayerJoined`,(data)=>{addChatMessage("join",data.username);});
+socket.on(`minecraftPlayerLeft`,(data)=>{addChatMessage("leave",data.username);});
+socket.on(`minecraftPlayerStats`,(data)=>{
+html.botStats.innerHTML=`${data.health.toFixed(2)} HP<br>X: ${data.xPos.toFixed(2)} Y: ${data.yPos.toFixed(2)} Z: ${data.zPos.toFixed(2)}`;
+});
+var pressedKeys=[];
+document.addEventListener("keydown",(e)=>{
+if(e.keyCode==17||e.ctrlKey)e.preventDefault();
+var key=e.key.toLowerCase();
+if(!pressedKeys.includes(key)){
+if(!key.startsWith("arrow"))pressedKeys.push(key);
+if(!controlsDisabled){
+if(key=="/"){html.gameChatInput.focus();}
+if(key=="t"){html.gameChatInput.focus();html.gameChatInput.value="";}
+if(key=="w"){socket.emit("minecraftAction","w");}
+if(key=="a"){socket.emit("minecraftAction","a");}
+if(key=="s"){socket.emit("minecraftAction","s");}
+if(key=="d"){socket.emit("minecraftAction","d");}
+if(key==" "){socket.emit("minecraftAction","jump");}
+if(key=="shift"){e.preventDefault();socket.emit("minecraftAction","sneak");}
+if(key=="control"){socket.emit("minecraftAction","sprint");}
+if(key=="arrowup"){socket.emit("minecraftAction","lookup");}
+if(key=="arrowleft"){socket.emit("minecraftAction","lookleft");}
+if(key=="arrowdown"){socket.emit("minecraftAction","lookdown");}
+if(key=="arrowright"){socket.emit("minecraftAction","lookright");}
+}}
+});
+document.addEventListener("keyup",(e)=>{
+var key=e.key.toLowerCase();
+var index=pressedKeys.indexOf(key);
+if(index!==-1){pressedKeys.splice(index,1);}
+if(key=="w"){socket.emit("minecraftStopAction","w");}
+if(key=="a"){socket.emit("minecraftStopAction","a");}
+if(key=="s"){socket.emit("minecraftStopAction","s");}
+if(key=="d"){socket.emit("minecraftStopAction","d");}
+if(key==" "){socket.emit("minecraftStopAction","jump");}
+if(key=="shift"){socket.emit("minecraftStopAction","sneak");}
+//if(key=="control"){socket.emit("minecraftStopAction","sprint");}
+});
+html.gameChatInput.onfocus=function(){controlsDisabled=true;};
+html.gameChatInput.onblur=function(){controlsDisabled=false;};
--- server.js
+++ server.js
@@ -8,16 +8,46 @@ require(`dotenv`);
 const port=8080;
 const fs=require(`fs`);
 var bots={};
+const lookModifier=0.1;
 
 io.on(`connection`,async socket=>{
 socket.on(`connectMinecraft`,async data=>{
-bots[socket.id]=new MinecraftBot(data.username,data.serverip,data.serverport,socket);
+bots[socket.id]=new MinecraftBot(data.username,data.serverip,data.serverport,socket,data.firstPerson);
 });
 socket.on(`sendChatMessage`,async data=>{
-if(bots[socket.id].bot){bots[socket.id].bot.chat(message);}
+if(bots[socket.id])if(bots[socket.id].bot)bots[socket.id].bot.chat(data);
+});
+socket.on(`minecraftAction`,async data=>{
+if(bots[socket.id]){
+if(bots[socket.id].bot){
+if(data=="w"){bots[socket.id].bot.setControlState("forward",true);}
+if(data=="a"){bots[socket.id].bot.setControlState("right",true);}
+if(data=="s"){bots[socket.id].bot.setControlState("back",true);}
+if(data=="d"){bots[socket.id].bot.setControlState("left",true);}
+if(data=="jump"){bots[socket.id].bot.setControlState("jump",true);}
+if(data=="sneak"){bots[socket.id].bot.setControlState("sneak",true);}
+if(data=="sprint"){bots[socket.id].bot.setControlState("sprint",true);}
+if(data=="lookup"){bots[socket.id].bot.look(bots[socket.id].bot.entity.yaw,bots[socket.id].bot.entity.pitch+lookModifier);}
+if(data=="lookleft"){bots[socket.id].bot.look(bots[socket.id].bot.entity.yaw+lookModifier,bots[socket.id].bot.entity.pitch);}
+if(data=="lookdown"){bots[socket.id].bot.look(bots[socket.id].bot.entity.yaw,bots[socket.id].bot.entity.pitch-lookModifier);}
+if(data=="lookright"){bots[socket.id].bot.look(bots[socket.id].bot.entity.yaw-lookModifier,bots[socket.id].bot.entity.pitch);}
+}}
+});
+socket.on(`minecraftStopAction`,async data=>{
+if(bots[socket.id]){
+if(bots[socket.id].bot){
+if(data=="w"){bots[socket.id].bot.setControlState("forward",false);}
+if(data=="a"){bots[socket.id].bot.setControlState("right",false);}
+if(data=="s"){bots[socket.id].bot.setControlState("back",false);}
+if(data=="d"){bots[socket.id].bot.setControlState("left",false);}
+if(data=="jump"){bots[socket.id].bot.setControlState("jump",false);}
+if(data=="sneak"){bots[socket.id].bot.setControlState("sneak",false);}
+if(data=="sprint"){bots[socket.id].bot.setControlState("sprint",!bots[socket.id].bot.getControlState("sprint"));}
+}}
 });
 socket.on(`disconnect`,async data=>{
-delete bots[socket.id]
+if(bots[socket.id]){if(bots[socket.id].bot){bots[socket.id].bot.viewer.close();bots[socket.id].bot.end();}}
+delete bots[socket.id];
 });
 });
 
@@ -29,31 +59,33 @@ res.sendFile(__dirname+`/${req.originalUrl}`);
 });
 http.listen(port,async ()=>{console.log(`Web server started at port ${port}`);});
 
-function MinecraftBot(username,serverip,serverport,socket){
+function MinecraftBot(username,serverip,serverport,socket,firstPerson){
 this.username=checkUsername(username);
 this.serverip=serverip;
 this.serverport=serverport||25565;
 this.socket=socket;
 this.viewerPort=random(10000,65535);
+this.isFirstPerson=firstPerson==true||false?firstPerson:false;
 this.bot=mineflayer.createBot({host:this.serverip,username:this.username,port:this.serverport});
 this.bot.on("kicked",(reason)=>{minecraftKicked(this.socket,reason);});
 this.bot.on("chat",(username,message)=>{minecraftChatMessage(this.socket,username,message);});
+this.bot.on("health",()=>{minecraftSendPlayerStats(this.socket,{health:this.bot.health,xPos:this.bot.entity.position.x,yPos:this.bot.entity.position.y,zPos:this.bot.entity.position.z});});
+this.bot.on("move",()=>{minecraftSendPlayerStats(this.socket,{health:this.bot.health,xPos:this.bot.entity.position.x,yPos:this.bot.entity.position.y,zPos:this.bot.entity.position.z});});
 this.bot.on("whisper",(username,message)=>{minecraftWhisperMessage(this.socket,username,message);});
-this.bot.on("playerJoined",(username,message)=>{minecraftPlayerJoined(this.socket,username);});
-this.bot.on("playerLeft",(username,message)=>{minecraftPlayerLeft(this.socket,username);});
+this.bot.on("playerJoined",(player)=>{minecraftPlayerJoined(this.socket,player);});
+this.bot.on("playerLeft",(player)=>{minecraftPlayerLeft(this.socket,player);});
 this.bot.on("error",(error)=>{minecraftError(this.socket,error);});
 this.bot.once("spawn",async()=>{
-await mineflayerViewer(this.bot,{port:this.viewerPort,firstPerson:false});
-sendConnectionInfo(this.socket,{url:`https://browser-minecraft.kimplayz4lk.repl.co:${this.viewerPort}`,username:this.bot.username,server:`${this.serverip}:${this.serverport}`});
+
+await mineflayerViewer(this.bot,{port:this.viewerPort,firstPerson:this.isFirstPerson});
+sendConnectionInfo(this.socket,{url:`http://browser-minecraft.herokuapp.com:${this.viewerPort}`,username:this.bot.username,server:`${this.serverip}:${this.serverport}`,health:this.bot.health,xPos:this.bot.entity.position.x,yPos:this.bot.entity.position.y,zPos:this.bot.entity.position.z});
 });
 }
 
 function random(min,max){return Math.floor(Math.random()*(max-min+1))+min;}
 function minecraftError(socket,error){socket.emit("minecraftConnectionError",`${error}`);}
-function minecraftKick(socket,error){socket.emit("minecraftConnectionKick",`${error}`);}
-function sendConnectionInfo(socket,data){
-socket.emit("minecraftConnection",data);
-}
+function minecraftKicked(socket,error){socket.emit("minecraftConnectionKicked",`${error}`);}
+function sendConnectionInfo(socket,data){socket.emit("minecraftConnection",data);}
 function checkUsername(username){
 if(username.length>=3&&username.length<=16)return username;
 if(username.length>16)return username.substring(0,16);
@@ -61,5 +93,6 @@ if(username.length<3)return `${ug.generateUsername("",12)}${random(0,9999)}`;
 }
 function minecraftChatMessage(socket,username,message){socket.emit("minecraftChatMessage",{username:username,message:message});}
 function minecraftWhisperMessage(socket,username,message){socket.emit("minecraftWhisperMessage",{username:username,message:message});}
-function minecraftPlayerJoined(socket,username){socket.emit("minecraftPlayerJoined",{username:username});}
-function minecraftPlayerLeft(socket,username){socket.emit("minecraftPlayerLeft",{username:username});}
+function minecraftPlayerJoined(socket,player){socket.emit("minecraftPlayerJoined",player);}
+function minecraftPlayerLeft(socket,player){socket.emit("minecraftPlayerLeft",player);}
+function minecraftSendPlayerStats(socket,data){socket.emit("minecraftPlayerStats",data);}
